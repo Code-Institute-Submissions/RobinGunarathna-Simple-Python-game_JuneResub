488a312cc8b369e242298635b8fb29ff83241ef5
RobinGunarathna <madgunrob@gmail.com>
2022-01-31 03:03:42 +0000

    Commit before deadline 2022-01-31

--- README.md
+++ README.md
@@ -1,31 +1,8 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# Just a simple Hangman game!
+I'm not gonna be able to get this to work before the deadline. :(
+I cant get the code to work right now there is a lot of mistakes i have to correct.
+I'm turning this in like this only because i have to so I can get my third attempt and i'm sorry to whoever is correcting this!
+I'll have everything done for my third attempt!
+Any feedback would be appriciated on how i can improve this or if i just should do a completly different project!
 
-Welcome USER_NAME,
-
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
-
-## Reminders
-
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
-
-## Creating the Heroku app
-
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
-
-1. `heroku/python`
-2. `heroku/nodejs`
-
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
-
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
-
-Connect your GitHub repository and deploy as normal.
-
-## Constraints
-
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
-
------
-Happy coding!
+#Thank you to who ever is correcting this and I am truly sorry!
--- run.py
+++ run.py
@@ -1,3 +1,140 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_complete = "_" * len(word)
+    guess = False
+    guess_letters = []
+    guess_words = []
+    attempts = 6
+    print("Play hangman!")
+    print(display_hangman(attempts))
+    print(word_complete)
+    print("\n")
+    while not guess and attempts > 0:
+        guessed = input("Guess a letter or a word: ").upper()
+        if len(guessed) == 1 and guessed.isalpha():
+            if guessed in guess_letters:
+                print("you allready guessed", guessed)
+            elif guessed not in word:
+                print(guessed, "is not in the word")
+                attempts -= 1
+                guess_letters.append(guessed)
+            else:
+                print("good!", guessed, "is in the word!")
+                guess_letters.append(guessed)
+                word_as_list = list(word_complete)
+                indices = [i for i, letter in enumerate(word) if letter == guessed]
+                for index in indices:
+                    word_as_list[index] = guessed
+                word_complete = "".join(word_as_list)
+                if "_" not in word_complete:
+                    guess = True
+        elif len(guessed) == len(word) and guessed.isalpha():
+            if guessed in guess_words:
+                print("you allready guessed the word", guessed)
+            elif guessed != word:
+                print(guessed, "is not the word")
+                attempts -= 1
+                guess_words.append(guessed)
+            else:
+                guess = True
+                word_complete = word
+        
+        else:
+            print("not a valid quess")
+        print(display_hangman(attempts))
+        print(word_complete)
+        print("\n")
+    if guess:
+        print("you guessed the word you win!")
+    else:
+        print("You ran out of attempts. The word was " + word + "Try again?")
+
+def display_hangman(attempts):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+def main():
+    word = get_word()
+    play(word)
+    while input("play again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)
+
+if __name__ == "__main__":
+    main()
