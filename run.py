d989ceeeabcd2c9a3423286d6ed5c316d2baa0b7
RobinGunarathna <madgunrob@gmail.com>
2022-02-02 16:02:29 +0000

    a lot of changes in code for heroku deployment

--- run.py
+++ run.py
@@ -1,140 +1,83 @@
 import random
-from words import word_list
+from constants import stage_dictionary as stages, word_list
 
 def get_word():
     word = random.choice(word_list)
     return word.upper()
 
+def is_word(_input):
+    if len(_input) > 1:
+        return True
+    else:
+        return False
+
 def play(word):
-    word_complete = "_" * len(word)
-    guess = False
-    guess_letters = []
-    guess_words = []
-    attempts = 6
+    word_as_list = list(word)
+
+    failed_attempts = []
+
+    guessed = False
+    guessed_letters = []
+
+    attempts = 0
+
     print("Play hangman!")
     print(display_hangman(attempts))
-    print(word_complete)
+    print(' '.join(char_ if char_ in guessed_letters else "_" for char_ in word_as_list))
     print("\n")
-    while not guess and attempts > 0:
-        guessed = input("Guess a letter or a word: ").upper()
-        if len(guessed) == 1 and guessed.isalpha():
-            if guessed in guess_letters:
-                print("you allready guessed", guessed)
-            elif guessed not in word:
-                print(guessed, "is not in the word")
-                attempts -= 1
-                guess_letters.append(guessed)
+
+    while not guessed and attempts < 6:
+        current_attempt = input("Guess a letter or a word: \n").upper()
+        if current_attempt.isdigit():
+            print(f"Numbers aren't allowed, try again.")
+            continue
+
+        if not is_word(current_attempt):
+
+            if current_attempt in guessed_letters or current_attempt in failed_attempts:
+                attempts += 1
+                print(f"You already inputted {current_attempt}")
+
+            elif current_attempt not in word:
+                print(f"{current_attempt}, is not in the word!")
+                attempts += 1
+                failed_attempts.append(current_attempt)
+
             else:
-                print("good!", guessed, "is in the word!")
-                guess_letters.append(guessed)
-                word_as_list = list(word_complete)
-                indices = [i for i, letter in enumerate(word) if letter == guessed]
-                for index in indices:
-                    word_as_list[index] = guessed
-                word_complete = "".join(word_as_list)
-                if "_" not in word_complete:
-                    guess = True
-        elif len(guessed) == len(word) and guessed.isalpha():
-            if guessed in guess_words:
-                print("you allready guessed the word", guessed)
-            elif guessed != word:
-                print(guessed, "is not the word")
-                attempts -= 1
-                guess_words.append(guessed)
-            else:
-                guess = True
-                word_complete = word
-        
+                print(f"Good!! {current_attempt} is in the word!")
+                guessed_letters.append(current_attempt)
+                current_word = [char_ if char_ in guessed_letters else "_" for char_ in word_as_list]
+
+                if "_" not in current_word:
+                    guessed = True
         else:
-            print("not a valid quess")
+            if current_attempt == word:
+                guessed = True
+            elif current_attempt in failed_attempts:
+                attempts += 1
+                print(f"You already tried the word {current_attempt}.")
+            else:
+                print(f"{current_attempt} is not the word.")
+                attempts += 1
+                failed_attempts.append(current_attempt)
+
         print(display_hangman(attempts))
-        print(word_complete)
+        print(' '.join(char_ if char_ in guessed_letters else "_" for char_ in word_as_list))
         print("\n")
-    if guess:
-        print("you guessed the word you win!")
+
+    if guessed:
+        print(f"You guessed the word > {word} < you win!")
     else:
-        print("You ran out of attempts. The word was " + word + "Try again?")
+        print(f"You ran out of attempts. The word was {word}. Try again?")
 
-def display_hangman(attempts):
-    stages = [  # final state: head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # head, torso, both arms, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / 
-                   -
-                """,
-                # head, torso, and both arms
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |      
-                   -
-                """,
-                # head, torso, and one arm
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |     
-                   -
-                """,
-                # head and torso
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |     
-                   -
-                """,
-                # head
-                """
-                   --------
-                   |      |
-                   |      O
-                   |    
-                   |      
-                   |     
-                   -
-                """,
-                # initial empty state
-                """
-                   --------
-                   |      |
-                   |      
-                   |    
-                   |      
-                   |     
-                   -
-                """
-    ]
-    return stages[tries]
+def display_hangman(attempt_number):
+    return stages[attempt_number]
 
-def main():
-    word = get_word()
-    play(word)
-    while input("play again? (Y/N) ").upper() == "Y":
-        word = get_word()
-        play(word)
 
 if __name__ == "__main__":
-    main()
+    word = get_word()
+    play(word)
+
+    while input("play again? (Y/N): \n").upper() == "Y":
+        word = get_word()
+        play(word)
